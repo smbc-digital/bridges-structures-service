d2394dea59e32bbff6970a057b771b806a03ab8c
Colin Lees <colinleesa7x@gmail.com>
2020-07-09 10:52:12 +0100

    fix: remove unneeded ComplaintMailModel

--- src/Helpers/MailHelper.cs
+++ src/Helpers/MailHelper.cs
@@ -1,7 +1,6 @@
 ﻿using bridges_structures_service.Models;
 using Newtonsoft.Json;
 using StockportGovUK.NetStandard.Gateways.MailingServiceGateway;
-using StockportGovUK.NetStandard.Models.ComplimentsComplaints;
 using StockportGovUK.NetStandard.Models.Enums;
 using StockportGovUK.NetStandard.Models.Mail;
 
@@ -18,19 +17,16 @@ namespace bridges_structures_service.Helpers
 
         public void SendEmail(Person person, EMailTemplate template, string caseReference)
         {
-            ComplaintsMailModel submissionDetails = new ComplaintsMailModel
-            {
-                Subject = "Bridges or Structures Report - submission",
-                Reference = caseReference,
-                FirstName = person.FirstName,
-                LastName = person.LastName,
-                RecipientAddress = person.Email
-            };
-
             _mailingServiceGateway.Send(new Mail
             {
-                Payload = JsonConvert.SerializeObject(submissionDetails),
-                Template = template
+                Template = template,
+                Payload = JsonConvert.SerializeObject(new {
+                    Subject = "Bridges or Structures Report - submission",
+                    Reference = caseReference,
+                    FirstName = person.FirstName,
+                    LastName = person.LastName,
+                    RecipientAddress = person.Email
+                })
             });
         }
     }
